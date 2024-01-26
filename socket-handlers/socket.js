4250e2c151a9026970e25c5e017a8a06bde01ba0
Logan Lagrange <lagrangelogan@gmail.com>
2024-01-25 21:17:14 -0500

    added room tracking

--- socket-handlers/socket.js
+++ socket-handlers/socket.js
@@ -2,24 +2,46 @@ const handleConnection = (io, socket) => {
     console.log(`User connected: ${socket.id}`)
 }
 
-const handleJoinRoom = (io, socket, room) => {
+const handleJoinRoom = (io, socket, room, roomData) => {
     socket.join(room);
     socket.emit(`Welcome to ${room}`)
+    if (!roomData[room]) {
+        roomData[room] = {count: 1}
+    } else {
+        roomData[room].count += 1;
+    }
+    io.to(room).emit("updateRoomData", roomData[room])
 }
 
-const handleMessage = (io, socket, room, message) => {
+const handleMessage = (io, socket, room, message, roomData) => {
     console.log(`Received message from ${socket.id}: ${message} in room ${room}`)
     io.to(room).emit('broadcastMessage', {message})
 }
 
-const handleDraw = (io, socket, room, change) => {
+const handleDraw = (io, socket, room, change, roomData) => {
     console.log(`Draw event from socket ${socket.id} in room: ${room}`)
     io.to(room).emit('drawChange', {change})
+}
+
+const handleLeave = (io, socket, room, roomData) => {
+    socket.leave(room);
+    console.log(`Socket ${socket} left room ${room}`);
+    if(roomData[room]) {
+        roomData[room].count -= 1;
+
+        if(roomData[room].count <= 0) {
+            delete roomData[room];
+        } else {
+            io.to(room).emit('updateRoomData', roomData[room])
+        }
+    }
+
 }
 
 module.exports = {
     handleConnection,
     handleJoinRoom,
     handleMessage,
-    handleDraw
+    handleDraw,
+    handleLeave
 }
--- socket-server.js
+++ socket-server.js
@@ -1,22 +1,27 @@
 const socketIO = require('socket.io');
-const {handleConnection, handleJoinRoom, handleMessage} = require('./socket-handlers/socket')
+const {handleConnection, handleJoinRoom, handleMessage, handleLeave} = require('./socket-handlers/socket')
 
 const initializeSocketServer = (server) => {
     const io = socketIO(server);
+    const roomData = {};
 
     io.on('connection', (socket) => {
-        handleConnection(io, socket)
+        handleConnection(io, socket, roomData)
 
         socket.on('join', (room) => {
-            handleJoinRoom(io, socket, room)
+            handleJoinRoom(io, socket, room, roomData)
         })
     
         socket.on('message', (room, message) => {
-            handleMessage(io, socket, room, message)
+            handleMessage(io, socket, room, message, roomData)
         })
 
         socket.on('draw', (room, change) => {
             handleDraw(io, socket, room, change)
+        })
+
+        socket.on('leave', (room) => {
+            handleLeave(io, socket, room, roomData)
         })
     })
 
